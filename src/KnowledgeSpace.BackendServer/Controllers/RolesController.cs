dd90ff404c33f300e995d5a12c0ebd894f9e68bd
Bach Ngoc Toan <ngoctoan@live.com>
2020-03-29 14:05:24 +0700

    Add REST API for Role

---
+++ src/KnowledgeSpace.BackendServer/Controllers/RolesController.cs
@@ -0,0 +1,146 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using KnowledgeSpace.ViewModels;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        //URL: POST: http://localhost:5001/api/roles
+        [HttpPost]
+        public async Task<IActionResult> PostRole(RoleVm roleVm)
+        {
+            var role = new IdentityRole()
+            {
+                Id = roleVm.Id,
+                Name = roleVm.Name,
+                NormalizedName = roleVm.Name.ToUpper()
+            };
+            var result = await _roleManager.CreateAsync(role);
+            if (result.Succeeded)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = role.Id }, roleVm);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+
+        //URL: GET: http://localhost:5001/api/roles/
+        [HttpGet]
+        public async Task<IActionResult> GetRoles()
+        {
+            var roles = await _roleManager.Roles
+                .Select(r => new RoleVm()
+                {
+                    Id = r.Id,
+                    Name = r.Name
+                })
+                .ToListAsync();
+
+            return Ok(roles);
+        }
+
+        //URL: GET: http://localhost:5001/api/roles/?filter={filter}&pageIndex=1&pageSize=10
+        [HttpGet]
+        public async Task<IActionResult> GetRoles(string filter, int pageIndex, int pageSize)
+        {
+            var query = _roleManager.Roles;
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(x => x.Id.Contains(filter) || x.Name.Contains(filter));
+            }
+            var totalRecords = await query.CountAsync();
+            var items = await query.Skip((pageIndex - 1 * pageSize))
+                .Take(pageSize)
+                .Select(r => new RoleVm()
+                {
+                    Id = r.Id,
+                    Name = r.Name
+                })
+                .ToListAsync();
+
+            var pagination = new Pagination<RoleVm>
+            {
+                Items = items,
+                TotalRecords = totalRecords,
+            };
+            return Ok(pagination);
+        }
+
+        //URL: GET: http://localhost:5001/api/roles/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            var roleVm = new RoleVm()
+            {
+                Id = role.Id,
+                Name = role.Name,
+            };
+            return Ok(roleVm);
+        }
+
+        //URL: PUT: http://localhost:5001/api/roles/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRole(string id, [FromBody]RoleVm roleVm)
+        {
+            if (id != roleVm.Id)
+                return BadRequest();
+
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            role.Name = roleVm.Name;
+            role.NormalizedName = roleVm.Name.ToUpper();
+
+            var result = await _roleManager.UpdateAsync(role);
+
+            if (result.Succeeded)
+            {
+                return NoContent();
+            }
+            return BadRequest(result.Errors);
+        }
+
+        //URL: DELETE: http://localhost:5001/api/roles/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            if (role == null)
+                return NotFound();
+
+            var result = await _roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                var rolevm = new RoleVm()
+                {
+                    Id = role.Id,
+                    Name = role.Name
+                };
+                return Ok(rolevm);
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}
---
+++ src/KnowledgeSpace.ViewModels/Pagination.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeSpace.ViewModels
+{
+    public class Pagination<T>
+    {
+        public List<T> Items { get; set; }
+
+        public int TotalRecords { get; set; }
+    }
+}
