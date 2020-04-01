2e9d1184440c1a6fdb99e3dad9f7372926b5cc10
Bach Ngoc Toan <ngoctoan@live.com>
2020-04-01 13:56:07 +0700

    Function API

---
+++ src/KnowledgeSpace.BackendServer/Controllers/FunctionsController.cs
@@ -0,0 +1,157 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using KnowledgeSpace.BackendServer.Data;
+using KnowledgeSpace.BackendServer.Data.Entities;
+using KnowledgeSpace.ViewModels;
+using KnowledgeSpace.ViewModels.Systems;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KnowledgeSpace.BackendServer.Controllers
+{
+    public class FunctionsController : BaseController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FunctionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostFunction([FromBody] FunctionCreateRequest request)
+        {
+            var function = new Function()
+            {
+                Id = request.Id,
+                Name = request.Name,
+                ParentId = request.ParentId,
+                SortOrder = request.SortOrder,
+                Url = request.Url
+            };
+            _context.Functions.Add(function);
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return CreatedAtAction(nameof(GetById), new { id = function.Id }, request);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFunctions()
+        {
+            var functions = _context.Functions;
+
+            var functionvms = await functions.Select(u => new FunctionVm()
+            {
+                Id = u.Id,
+                Name = u.Name,
+                Url = u.Url,
+                SortOrder = u.SortOrder,
+                ParentId = u.ParentId
+            }).ToListAsync();
+
+            return Ok(functionvms);
+        }
+
+        [HttpGet("filter")]
+        public async Task<IActionResult> GetFunctionsPaging(string filter, int pageIndex, int pageSize)
+        {
+            var query = _context.Functions.AsQueryable();
+            if (!string.IsNullOrEmpty(filter))
+            {
+                query = query.Where(x => x.Name.Contains(filter)
+                || x.Id.Contains(filter)
+                || x.Url.Contains(filter));
+            }
+            var totalRecords = await query.CountAsync();
+            var items = await query.Skip((pageIndex - 1 * pageSize))
+                .Take(pageSize)
+                .Select(u => new FunctionVm()
+                {
+                    Id = u.Id,
+                    Name = u.Name,
+                    Url = u.Url,
+                    SortOrder = u.SortOrder,
+                    ParentId = u.ParentId
+                })
+                .ToListAsync();
+
+            var pagination = new Pagination<FunctionVm>
+            {
+                Items = items,
+                TotalRecords = totalRecords,
+            };
+            return Ok(pagination);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var function = await _context.Functions.FindAsync(id);
+            if (function == null)
+                return NotFound();
+
+            var functionVm = new FunctionVm()
+            {
+                Id = function.Id,
+                Name = function.Name,
+                Url = function.Url,
+                SortOrder = function.SortOrder,
+                ParentId = function.ParentId
+            };
+            return Ok(functionVm);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFunction(string id, [FromBody]FunctionCreateRequest request)
+        {
+            var function = await _context.Functions.FindAsync(id);
+            if (function == null)
+                return NotFound();
+
+            function.Name = request.Name;
+            function.ParentId = request.ParentId;
+            function.SortOrder = request.SortOrder;
+            function.Url = request.Url;
+
+            _context.Functions.Update(function);
+            var result = await _context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFunction(string id)
+        {
+            var function = await _context.Functions.FindAsync(id);
+            if (function == null)
+                return NotFound();
+
+            _context.Functions.Remove(function);
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                var functionvm = new FunctionVm()
+                {
+                    Id = function.Id,
+                    Name = function.Name,
+                    Url = function.Url,
+                    SortOrder = function.SortOrder,
+                    ParentId = function.ParentId
+                };
+                return Ok(functionvm);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ src/KnowledgeSpace.ViewModels/Systems/FunctionCreateRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KnowledgeSpace.ViewModels.Systems
+{
+    public class FunctionCreateRequestValidator : AbstractValidator<FunctionCreateRequest>
+    {
+        public FunctionCreateRequestValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id value is required")
+               .MaximumLength(50).WithMessage("Function Id cannot over limit 50 characters");
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name value is required")
+              .MaximumLength(200).WithMessage("Name cannot over limit 200 characters");
+
+            RuleFor(x => x.Url).NotEmpty().WithMessage("URL value is required")
+             .MaximumLength(200).WithMessage("URL cannot over limit 200 characters");
+
+            RuleFor(x => x.ParentId).MaximumLength(50)
+                .When(x => !string.IsNullOrEmpty(x.ParentId))
+                .WithMessage("URL cannot over limit 50 characters");
+        }
+    }
+}
