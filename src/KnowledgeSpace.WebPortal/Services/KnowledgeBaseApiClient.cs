aada16d6a157d25f950b50bed047adc869620ecc
Bach Ngoc Toan <ngoctoan@live.com>
2020-04-27 14:24:52 +0700

    Refactoring api client code

--- src/KnowledgeSpace.WebPortal/Services/KnowledgeBaseApiClient.cs
+++ src/KnowledgeSpace.WebPortal/Services/KnowledgeBaseApiClient.cs
@@ -1,108 +1,62 @@
 ﻿using KnowledgeSpace.ViewModels;
 using KnowledgeSpace.ViewModels.Contents;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace KnowledgeSpace.WebPortal.Services
 {
-    public class KnowledgeBaseApiClient : IKnowledgeBaseApiClient
+    public class KnowledgeBaseApiClient : BaseApiClient, IKnowledgeBaseApiClient
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IConfiguration _configuration;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-
         public KnowledgeBaseApiClient(IHttpClientFactory httpClientFactory,
-            IConfiguration configuration,
-            IHttpContextAccessor httpContextAccessor)
+          IConfiguration configuration,
+          IHttpContextAccessor httpContextAccessor) : base(httpClientFactory, configuration, httpContextAccessor)
         {
-            _httpClientFactory = httpClientFactory;
-            _configuration = configuration;
-            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<KnowledgeBaseVm> GetKnowledgeBaseDetail(int id)
         {
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync($"/api/knowledgeBases/{id}");
-            var knowledgeBase = JsonConvert.DeserializeObject<KnowledgeBaseVm>(await response.Content.ReadAsStringAsync());
-            return knowledgeBase;
+            return await GetAsync<KnowledgeBaseVm>($"/api/knowledgeBases/{id}");
         }
 
         public async Task<Pagination<KnowledgeBaseQuickVm>> GetKnowledgeBasesByCategoryId(int categoryId, int pageIndex, int pageSize)
         {
             var apiUrl = $"/api/knowledgeBases/filter?categoryId={categoryId}&pageIndex={pageIndex}&pageSize={pageSize}";
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync(apiUrl);
-            var knowledgeBases = JsonConvert.DeserializeObject<Pagination<KnowledgeBaseQuickVm>>(await response.Content.ReadAsStringAsync());
-            return knowledgeBases;
+            return await GetAsync<Pagination<KnowledgeBaseQuickVm>>(apiUrl);
         }
 
         public async Task<Pagination<KnowledgeBaseQuickVm>> GetKnowledgeBasesByTagId(string tagId, int pageIndex, int pageSize)
         {
             var apiUrl = $"/api/knowledgeBases/tags/{tagId}?pageIndex={pageIndex}&pageSize={pageSize}";
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync(apiUrl);
-            var knowledgeBases = JsonConvert.DeserializeObject<Pagination<KnowledgeBaseQuickVm>>(await response.Content.ReadAsStringAsync());
-            return knowledgeBases;
+            return await GetAsync<Pagination<KnowledgeBaseQuickVm>>(apiUrl);
         }
 
         public async Task<List<LabelVm>> GetLabelsByKnowledgeBaseId(int id)
         {
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync($"/api/knowledgeBases/{id}/labels");
-            var labels = JsonConvert.DeserializeObject<List<LabelVm>>(await response.Content.ReadAsStringAsync());
-            return labels;
+            return await GetListAsync<LabelVm>($"/api/knowledgeBases/{id}/labels");
         }
 
         public async Task<List<KnowledgeBaseQuickVm>> GetLatestKnowledgeBases(int take)
         {
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            //var token = await _httpContextAccessor.HttpContext.GetTokenAsync("access_token");
-            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var response = await client.GetAsync($"/api/knowledgeBases/latest/{take}");
-            var latestKnowledgeBases = JsonConvert.DeserializeObject<List<KnowledgeBaseQuickVm>>(await response.Content.ReadAsStringAsync());
-            return latestKnowledgeBases;
+            return await GetListAsync<KnowledgeBaseQuickVm>($"/api/knowledgeBases/latest/{take}");
         }
 
         public async Task<List<KnowledgeBaseQuickVm>> GetPopularKnowledgeBases(int take)
         {
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync($"/api/knowledgeBases/popular/{take}");
-            var latestKnowledgeBases = JsonConvert.DeserializeObject<List<KnowledgeBaseQuickVm>>(await response.Content.ReadAsStringAsync());
-            return latestKnowledgeBases;
+            return await GetListAsync<KnowledgeBaseQuickVm>($"/api/knowledgeBases/popular/{take}");
         }
 
         public async Task<List<CommentVm>> GetRecentComments(int take)
         {
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync($"/api/knowledgeBases/comments/recent/{take}");
-            var comments = JsonConvert.DeserializeObject<List<CommentVm>>(await response.Content.ReadAsStringAsync());
-            return comments;
+            return await GetListAsync<CommentVm>($"/api/knowledgeBases/comments/recent/{take}");
         }
 
         public async Task<Pagination<KnowledgeBaseQuickVm>> SearchKnowledgeBase(string keyword, int pageIndex, int pageSize)
         {
             var apiUrl = $"/api/knowledgeBases/filter?filter={keyword}&pageIndex={pageIndex}&pageSize={pageSize}";
-            var client = _httpClientFactory.CreateClient();
-            client.BaseAddress = new Uri(_configuration["BackendApiUrl"]);
-            var response = await client.GetAsync(apiUrl);
-            var knowledgeBases = JsonConvert.DeserializeObject<Pagination<KnowledgeBaseQuickVm>>(await response.Content.ReadAsStringAsync());
-            return knowledgeBases;
+            return await GetAsync<Pagination<KnowledgeBaseQuickVm>>(apiUrl);
         }
     }
 }
