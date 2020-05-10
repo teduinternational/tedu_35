d394117b11d1147c727fd08830446c9dd04c1376
Bach Ngoc Toan <ngoctoan@live.com>
2020-05-10 11:46:15 +0700

    Add security headers

--- src/KnowledgeSpace.BackendServer/Startup.cs
+++ src/KnowledgeSpace.BackendServer/Startup.cs
@@ -174,7 +174,21 @@ namespace KnowledgeSpace.BackendServer
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHsts(hsts => hsts.MaxAge(365).IncludeSubdomains().Preload());
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opts => opts.NoReferrer());
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());
+            app.UseXfo(options => options.Deny());
+            app.UseCsp(opts => opts
+                    .BlockAllMixedContent()
+                    .StyleSources(s => s.Self())
+                    .StyleSources(s => s.UnsafeInline())
+                    .FontSources(s => s.Self())
+                    .FormActions(s => s.Self())
+                    .FrameAncestors(s => s.Self())
+                    .ImageSources(s => s.Self())
+                    .ScriptSources(s => s.Self())
+                );
             app.UseErrorWrapping();
             app.UseStaticFiles();
 
